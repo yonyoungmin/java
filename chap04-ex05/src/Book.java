c23e4e80024435e20c58de9cad29d07ce1ebb221
USER <USER@AMP-15>
2019-03-21 12:33:05 +0900

    Commit

---
+++ chap04-ex05/src/Book.java
@@ -0,0 +1,36 @@
+
+public class Book {
+	String title;
+	String author;
+	
+	void show(){
+        System.out.println(title + " " + author);
+	}
+     
+	public Book() {
+		this(" ", " ");
+		
+	}
+	
+	
+	public Book(String title){
+		this(title, "작자미상");
+	}
+	
+	public Book(String title, String author){
+		this.title = title;
+		this.author = author;
+		
+	}
+        
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Book littlePrince = new Book("어린이","유정재");
+        Book loveStory = new Book("간영웅전");
+        Book emptyBook = new Book();
+        littlePrince.show();
+        loveStory.show();
+        emptyBook.show();
+	} 
+
+}
