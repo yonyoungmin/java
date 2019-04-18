d35b54e98995780828520a66e35f5e69a2f87218
USER <USER@AMP-25>
2019-04-18 15:17:15 +0900

    Commit

---
+++ chap09-ex05/src/GridLayoutEx.java
@@ -0,0 +1,35 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class GridLayoutEx extends JFrame{
+	public GridLayoutEx(){
+		setTitle("GirdLayout Sample");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		GridLayout grid = new GridLayout(4, 2);
+		grid.setVgap(5);
+		
+		Container c = getContentPane();
+		c.setLayout(grid);
+		c.add(new JLabel(" 이름"));
+		c.add(new JTextField(""));
+		c.add(new JLabel(" 학번"));
+		c.add(new JTextField(""));
+		c.add(new JLabel(" 학과"));
+		c.add(new JTextField(""));
+		c.add(new JLabel(" 과목"));
+		c.add(new JTextField(""));
+		setSize(300, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new GridLayoutEx();
+	}
+
+}
