afac86c0518f88031f5c4cb90887125a76e5221a
USER <USER@AMP-25>
2019-04-18 15:33:07 +0900

    Commit

---
+++ chap09-ex02/src/ContentPaneEx.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+public class ContentPaneEx extends JFrame{
+	public ContentPaneEx(){
+		setTitle("Content pane and JFame");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentpane = getContentPane();
+		contentpane.setBackground(Color.RED);
+		contentpane.setLayout(new FlowLayout());
+		
+		contentpane.add(new JButton("OK"));
+		contentpane.add(new JButton("CANCEL"));
+		contentpane.add(new JButton("IGNORE"));
+		
+		setSize(300,200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new ContentPaneEx();
+	}
+
+}
