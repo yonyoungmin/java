90a60f68e8079a5f65947183b15e734608a5d41a
USER <USER@AMP-25>
2019-04-18 19:16:14 +0900

    Commit

---
+++ chap09-fig9-11/src/BorderLayoutSampleEx.java
@@ -0,0 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class BorderLayoutSampleEx extends JFrame{
+	public BorderLayoutSampleEx(){
+		setTitle("BorderLayout Samples");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new BorderLayout(30, 20));
+		
+		c.add(new JButton("add"), BorderLayout.NORTH);
+	    c.add(new JButton("sub"), BorderLayout.SOUTH);
+	    c.add(new JButton("mul"), BorderLayout.EAST);
+		c.add(new JButton("div"), BorderLayout.WEST);
+		c.add(new JButton("calculate"), BorderLayout.CENTER);
+		
+		setSize(300, 200);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new BorderLayoutSampleEx();
+	}
+}
