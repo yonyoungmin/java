c54def2f2d70bfd37cfef2e6d2079f8387619bfa
USER <USER@AMP-25>
2019-04-22 18:39:10 +0900

    Commit

---
+++ chap10-ex03/src/AnonymousClassListener.java
@@ -0,0 +1,39 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class AnonymousClassListener extends JFrame{
+	public AnonymousClassListener(){
+		setTitle("Action 이벤트 리스너 작성");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		c.add(btn);
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JButton b = (JButton)e.getSource();
+				if(b.getText().equals("Action"))
+					b.setText("액션");
+				else
+					b.setText("Action");
+				
+				setTitle(b.getText());
+			}
+		});
+		
+		setSize(350, 150);
+		setVisible(true);
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new AnonymousClassListener();
+	}
+
+}
