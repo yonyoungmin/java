8a82f26b66f0d840eaaf944fb12b00ac0d1f4e10
USER <USER@AMP-25>
2019-04-24 18:17:52 +0900

    Commit

---
+++ chap11-ex06/src/RadioButtonEx.java
@@ -0,0 +1,46 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+
+public class RadioButtonEx extends JFrame{
+	public RadioButtonEx(){
+		setTitle("라디오버튼 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		ImageIcon cherryIcon = new ImageIcon("images/cherry.jpg");
+		ImageIcon selectedCherryIcon = new ImageIcon("images/selectedCharry.jpg");
+		
+		
+		ButtonGroup g = new ButtonGroup();
+		
+		JRadioButton apple = new JRadioButton("사과");
+		JRadioButton pear = new JRadioButton("배", true);
+		JRadioButton cherry = new JRadioButton("체리", cherryIcon);
+		
+		cherry.setBorderPainted(true);
+		cherry.setSelectedIcon(selectedCherryIcon);
+		
+		g.add(apple);
+		g.add(pear);
+		g.add(cherry);
+		
+		c.add(apple);
+		c.add(pear);
+		c.add(cherry);
+		
+		setSize(250,150);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new RadioButtonEx();
+	}
+
+}
