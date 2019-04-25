19cc616d34cd5c9f6a103b5690bd8d234302e304
USER <USER@AMP-25>
2019-04-25 15:14:23 +0900

    Commit

---
+++ chap12-ex04/src/GraphicsDrawLineEx.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDrawLineEx extends JFrame{
+	private Mypanel panel = new Mypanel();
+	
+	public GraphicsDrawLineEx(){
+		setTitle("drawLine 사용 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(200,170);
+		setVisible(true);
+	}
+	
+	class Mypanel extends JPanel{
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			g.setColor(Color.RED);
+			g.drawLine(20, 20, 100, 100);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new GraphicsDrawLineEx();
+	}
+
+}
