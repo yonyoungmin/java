1785a25d70d1141e0a8b84fc8a5df94d99f69fc9
USER <USER@AMP-25>
2019-04-26 18:18:46 +0900

    Commit

---
+++ chap12-fig12-02-drawOval/src/GraphicsDrawOvalEx.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicsDrawOvalEx extends JFrame{
+	private MyPanel panel = new MyPanel();
+	
+	public GraphicsDrawOvalEx(){
+		setTitle("drawOval");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(200,170);
+		setVisible(true);
+	}
+	
+	class MyPanel extends JPanel{
+		public void paintComponent(Graphics g){
+			g.setColor(Color.RED);
+			g.drawOval(20,20, 80, 80);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        new GraphicsDrawOvalEx();
+	}
+
+}
