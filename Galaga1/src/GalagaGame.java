da5746efcbddab5d09808c545e367256564d8e89
USER <USER@AMP-25>
2019-05-09 10:50:19 +0900

    Commit

--- Galaga1/src/GalagaGame.java
+++ Galaga1/src/GalagaGame.java
@@ -11,32 +11,34 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GalagaGame extends JPanel implements KeyListener{
-	
+public class GalagaGame extends JPanel implements KeyListener {
+
 	private boolean running = true;
-	
+
 	private ArrayList sprites = new ArrayList();
 	private Sprite starship;
-	
+
+	private BufferedImage CheongImage;
 	private BufferedImage alienImage;
 	private BufferedImage shotImage;
 	private BufferedImage shipImage;
-	
-	public GalagaGame(){
+
+	public GalagaGame() {
 		JFrame frame = new JFrame("Galaga Game");
-		
+
 		frame.setSize(800, 600);
 		frame.add(this);
 		frame.setResizable(false);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		try{
+
+		try {
+			CheongImage = ImageIO.read(new File("Cheong.png"));
 			shotImage = ImageIO.read(new File("fire.png"));
 			shipImage = ImageIO.read(new File("starship.png"));
 			alienImage = ImageIO.read(new File("alien.png"));
-			
-		}catch (IOException e){
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		this.requestFocus();
@@ -51,94 +53,103 @@ public class GalagaGame extends JPanel implements KeyListener{
 			for (int x = 0; x < 12; x++) {
 				Sprite alien = new AlienSprite(this, alienImage, 100 + (x * 50), (50) + y * 30);
 				sprites.add(alien);
-				
-			    }
+				Sprite Cheong = new AlienSprite(this, CheongImage, 100 + (x * 100), (100) + y * 10);
+				sprites.add(Cheong);
+
 			}
-		}
-
-		private void startGame() {
-			sprites.clear();
-			initSprites();
-			
-		}
-		
-		public void endGame() {
-			System.exit(0);
-		}
-
-		public void removeSprite(Sprite sprite) {
-			sprites.remove(sprite);
-		}
-
-		public void fire() {
-			ShotSprite shot = new ShotSprite(this, shotImage, starship.getX() + 10, starship.getY() - 30);
-			sprites.add(shot);
-		}
-
-		@Override
-		public void paint(Graphics g) {
-			super.paint(g);
-			g.setColor(Color.black);
-			g.fillRect(0, 0, 800, 600);
-			for (int i = 0; i < sprites.size(); i++) {
-				Sprite sprite = (Sprite) sprites.get(i);
-				sprite.draw(g);
-			}
-		}
-			
-		public void gameLoop() {
-
-			while (running) {
-				for (int i = 0; i < sprites.size(); i++) {
-					Sprite sprite = (Sprite) sprites.get(i);
-					sprite.move();
-				}
-
-				for (int p = 0; p < sprites.size(); p++) {
-					for (int s = p + 1; s < sprites.size(); s++) {
-						Sprite me = (Sprite) sprites.get(p);
-						Sprite other = (Sprite) sprites.get(s);
-
-						if (me.checkCollision(other)) {
-							me.handleCollision(other);
-							other.handleCollision(me);
-						}
-					}
-				}
-				
-				repaint();
-				try {
-					Thread.sleep(10);
-				} catch (Exception e) {
-				}
-			}
-		}
-
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if (e.getKeyCode() == KeyEvent.VK_LEFT)
-				starship.setDx(-3);
-			if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-				starship.setDx(+3);
-			if (e.getKeyCode() == KeyEvent.VK_SPACE)
-				fire();
-		}
-
-		@Override
-		public void keyReleased(KeyEvent e) {
-			if (e.getKeyCode() == KeyEvent.VK_LEFT)
-				starship.setDx(0);
-			if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-				starship.setDx(0);
-		}
-
-		@Override
-		public void keyTyped(KeyEvent arg0) {
-		}
-
-		public static void main(String argv[]) {
-			GalagaGame g = new GalagaGame();
-			g.gameLoop();
 		}
 	}
 
+	private void startGame() {
+		sprites.clear();
+		initSprites();
+
+	}
+
+	public void endGame() {
+		System.exit(0);
+	}
+
+	public void removeSprite(Sprite sprite) {
+		sprites.remove(sprite);
+	}
+
+	public void fire() {
+		ShotSprite shot = new ShotSprite(this, shotImage, starship.getX() + 10, starship.getY() - 20);
+		sprites.add(shot);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(Color.pink);
+		g.fillRect(0, 0, 800, 600);
+		for (int i = 0; i < sprites.size(); i++) {
+			Sprite sprite = (Sprite) sprites.get(i);
+			sprite.draw(g);
+		}
+	}
+
+	public void gameLoop() {
+
+		while (running) {
+			for (int i = 0; i < sprites.size(); i++) {
+				Sprite sprite = (Sprite) sprites.get(i);
+				sprite.move();
+			}
+
+			for (int p = 0; p < sprites.size(); p++) {
+				for (int s = p + 1; s < sprites.size(); s++) {
+					Sprite me = (Sprite) sprites.get(p);
+					Sprite other = (Sprite) sprites.get(s);
+
+					if (me.checkCollision(other)) {
+						me.handleCollision(other);
+						other.handleCollision(me);
+					}
+				}
+			}
+
+			repaint();
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			starship.setDx(-3);
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			starship.setDx(+3);
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+			starship.setDy(-3);
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			starship.setDy(+3);
+		if (e.getKeyCode() == KeyEvent.VK_SPACE)
+			fire();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			starship.setDx(0);
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			starship.setDx(0);
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+			starship.setDy(0);
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			starship.setDy(0);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+	}
+
+	public static void main(String argv[]) {
+		GalagaGame g = new GalagaGame();
+		g.gameLoop();
+	}
+}
