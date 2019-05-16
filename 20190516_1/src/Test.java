082df0b824baa7b77f6d4de513fe943c8a499192
USER <USER@AMP-15>
2019-05-16 10:56:15 +0900

    Commit

---
+++ 20190516_1/src/Test.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Test extends JFrame{
+	private class radius{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		
+    Scanner a = new Scanner(System.in);
+    Scanner b = new Scanner(System.in);
+   
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    
+    c.add(new JButton("0"));
+    c.add(new JButton("1"));
+    c.add(new JButton("2"));
+    c.add(new JButton("3"));
+    c.add(new JButton("4"));
+    c.add(new JButton("5"));
+    c.add(new JButton("6"));
+    c.add(new JButton("7"));
+    c.add(new JButton("8"));
+    c.add(new JButton("9"));
+    c.add(new JButton("+"));
+    c.add(new JButton("-"));
+    c.add(new JButton("*"));
+    c.add(new JButton("/"));
+    c.add(new JButton("="));
+    
+	}
+
+}
